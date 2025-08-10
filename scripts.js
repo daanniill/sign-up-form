fb776afac1d5ae8ce6cd8838fc1972c1be7b6a80
daanniill <daniil.malakhov12@gmail.com>
2025-08-10 14:48:44 -0700

    added animation to the labels of inputs

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function btn_anim() {
+    const inputs = document.querySelectorAll("input")
+    inputs.forEach(input => {
+        const label = input.previousElementSibling
+        input.addEventListener('focus', () => {
+            label.style.transform = 'translateX(0) translateY(0)'
+            label.style.fontSize = '16px';
+            input.style.color = '#040404'
+            label.style.transition = '0.2s'
+        })
+        input.addEventListener('blur', () => {
+            // Only move back if input is empty
+            if (input.value.trim() === '') {
+                label.style.transform = 'translateX(10px) translateY(28px)';
+                label.style.fontSize = '16px';
+                label.style.transition = '0.2s'
+            }
+            input.style.color = 'white'
+        })
+    })
+}
+btn_anim()
